823682617f320826d27a2b6e0179546be955ac48
Brahmaiah Talluri <159916732+brahmaiahtalluri@users.noreply.github.com>
2025-09-16 14:06:58 +0530

    Update Problem_3.java

--- Problem_3.java
+++ Problem_3.java
@@ -1,19 +1,18 @@
 import java.util.*;
 public class Problem_3 {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int c=a%2==0?a-1:a;
-		for(int i=0;i<=c;i++) {
-			int odd=2*i+1;
-			System.out.print(odd);
-			
-			if(i<a-1) {
-				System.out.print(",");
-			}
-		}
-		sc.close();
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
+        int limit = (a % 2 == 0) ? a - 1 : a;
+
+        for (int i = 0; i < limit; i++) {
+            int odd = 2 * i + 1;
+            System.out.print(odd);
+            if (i < limit - 1) {
+                System.out.print(",");
+            }
+        }
+        sc.close();
+    }
 }
